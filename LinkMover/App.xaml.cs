897c5132861d9663369a6507f88737f251e14e69
Henning Entz <entz.henning@gmail.com>
2026-04-28 11:17:52 +0200

    chore: initial WPF project scaffolding
    Visual Studio template baseline: net10.0-windows, UseWPF, single
    LinkMover project with empty MainWindow. Starting point before
    PRISM/MVVM modularization.

---
+++ LinkMover/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LinkMover;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
